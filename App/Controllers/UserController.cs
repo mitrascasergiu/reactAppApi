23054187aa178c66aba2c2591949b9fa47a1abe5
mitrascasergiu <mitrasca.sergiu@gmail.com>
2022-10-16 16:38:23 +0300

    initial commit

---
+++ App/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public LoginResponse Login([FromBody] LoginRequest request)
+        {
+            //mocks
+            if (request.Username.Equals("admin"))
+            {
+                return new LoginResponse()
+                {
+                    Username = request.Username,
+                    Role = "admin"
+                };
+            } else if (request.Username.Equals("user"))
+            {
+                return new LoginResponse()
+                {
+                    Username = request.Username,
+                    Role = "user"
+                };
+            } else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
+
---
+++ App/Models/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Models
+{
+    public class LoginResponse
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
+
